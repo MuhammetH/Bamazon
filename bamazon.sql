1a2233c2e3eb8b8f9b2a818e6c67d0768b712db8
MuhammetD <doktor.oglu333@gmail.com>
2019-05-29 14:50:36 -0500

    added note 8 to products

--- bamazon.sql
+++ bamazon.sql
@@ -1,6 +1,9 @@
 /*Creating database and table*/
 CREATE DATABASE Bamazon;
 USE Bamazon;
+
+DROP TABLE IF EXISTS `Products`;
+
 CREATE TABLE Products(
 	itemID INTEGER(10) AUTO_INCREMENT NOT NULL,
     productName VARCHAR(50) NOT NULL,
@@ -9,3 +12,5 @@ CREATE TABLE Products(
     stockQuantity INTEGER(10),
     primary key (ItemId)
 );
+
+INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Galaxy Note 8','electronics',449.99,4);
