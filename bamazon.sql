90f5a5b4f0fe44d4c18569bc94a58f76708371e4
MuhammetD <doktor.oglu333@gmail.com>
2019-05-29 15:16:58 -0500

    finished adding products

--- bamazon.sql
+++ bamazon.sql
@@ -18,8 +18,8 @@ INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Gal
 INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('IPhone XS max','electronics',599.99,1);
 INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Galaxy s10+ (new)','electronics',999.99,1);
 INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Galaxy s10','electronics',599.99,1);
-INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Galaxy Note 9','electronics',549.99,2);
-INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Galaxy Note 9','electronics',549.99,2);
-INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Galaxy Note 9','electronics',549.99,2);
-INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Galaxy Note 9','electronics',549.99,2);
+INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Gear s3 Frontier','electronics',239.99,3);
+INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Galaxy Tab s4','electronics',710.00,2);
+INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Logitech G502 Wireless','electronics',144.99,2);
+INSERT INTO Products(productName,departmentName,price,stockQuantity) VALUES('Sony Hear-ON 2','electronics',199.99,6);
 
